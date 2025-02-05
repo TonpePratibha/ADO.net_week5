8a609bc58a136856bbf0422097f57b0c67909afc
TonpePratibha <pratibhatonpe.12345@gmail.com>
2025-02-05 11:56:08 +0530

    reflectioand delegates

---
+++ ReflectionAndDelegats/ReflectionAndDelegats/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Reflection;
+
+
+
+class Student {
+    public int Rollno{ get; set; }
+    public string Name { get; set; }
+   public int marks { get; set; }
+
+    public void DiaplayData() {
+
+        Console.WriteLine(Rollno + Name + marks);
+
+    }
+    public Student() { 
+    Rollno = 0;
+        Name = "pratibha";
+        marks = 100;
+    }
+
+    public Student(int rollno, string name,int marks) { 
+
+        Rollno = rollno;
+            Name = name;
+        marks = marks;
+    }
+
+}
+
+internal class Program
+{
+
+
+    private static void Main(string[] args)
+    {
+       /* Type type = typeof(string);
+        Console.WriteLine(type.Name);
+        MethodInfo[] methods = type.GetMethods();
+        foreach (var method in methods) { 
+        Console.WriteLine(method.Name);
+            ParameterInfo[] parameters=method.GetParameters();
+            foreach (var param in parameters) { 
+            Console.WriteLine($"{param.Name}");
+            }
+
+        }*/
+
+        Assembly assembly = Assembly.GetExecutingAssembly();
+        Type[] types= assembly.GetTypes();
+        foreach (Type type in types)
+        {
+            Console.WriteLine(type.Name);
+
+
+
+            MethodInfo[] methods = type.GetMethods();
+            foreach (MethodInfo method in methods) { 
+            Console.WriteLine($"{method.Name}");
+
+                ParameterInfo[] parameters=method.GetParameters();
+              foreach(ParameterInfo parameter in parameters)  {
+                    Console.WriteLine(parameter.Name);
+                }
+            }
+
+        }
+    }
+}
---
+++ ReflectionAndDelegats/delegates/Program.cs
@@ -0,0 +1,41 @@
+﻿
+
+
+delegate void MyDel(int x,int y);
+internal class Program
+{
+    public static void Add(int x, int y) 
+     {
+        //return x + y;
+        Console.WriteLine(x + y);
+    }
+    public static void multiply(int x, int y)
+    {
+        // return x * y;
+        Console.WriteLine(x * y);
+    }
+    private static void Main(string[] args)
+    {
+        /*  MyDel del = new MyDel(Add);
+          int result = del(12, 23);
+          Console.WriteLine(result);
+          del = multiply ;
+          result= del(12, 23);
+          Console.WriteLine(result);
+
+
+          //another way
+          MyDel del1 = new MyDel(Add);  //it will gives th last function result it overrides previous result
+          int result1 = del(10,20);
+          del1 += multiply;
+          Console.WriteLine(result1);
+        */
+        //to avoid above we ca ue nother way by making function return types to void
+        MyDel del = Add;
+        del += multiply;
+        del(12, 20);
+       
+
+            
+    }
+}
